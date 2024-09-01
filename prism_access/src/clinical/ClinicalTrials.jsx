cbeec88513e9a9bd73137b3485db85421c298bb7
manjunathBase2 <manjunathworkspace@gmail.com>
2024-09-02 00:51:22 +0530

    refactor: Update API endpoints to use production URLs

--- prism_access/src/clinical/ClinicalTrials.jsx
+++ prism_access/src/clinical/ClinicalTrials.jsx
@@ -14,9 +14,9 @@ function ClinicalTrials() {
     const handleFilter = async () => {
         try {
             const response = await axios.post(
-                // `http://localhost:5000/studies/`,
+                `http://localhost:5000/studies/`,
                 // `http://10.146.71.0:5000/studies/`,
-                `/studies/`,
+                // `/studies/`,
                 // `https://rr-backend-m7hi.onrender.com/studies/`,
                 // 'https://drug-reimbursement-regulatory-status.roche.com/443/studies/',
 
